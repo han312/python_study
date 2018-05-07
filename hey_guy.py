b46311462e968fad8958b6d7aece3c933bb89a14
han <changhan312@163.com>
2018-05-07 22:07:07 +0800

    Add files via upload

---
+++ hey_guy.py
@@ -0,0 +1,111 @@
+# message="hey guy"
+# print(message)
+# print("hello world")
+# name="chang han"
+# print(name)
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+# xing="chang"
+# ming="han"
+# xingming=xing.title()+" "+ming.title()
+# print(xingming)
+# l="  111  "
+# l.rstrip()
+# l=l.strip()
+# print(l)
+# print("one of python's strengths is its diverse community")
+# print('kongzhi said, "有朋自远方来，不亦乐乎"')
+# import this
+# bicycles=['trek','cannodale','redline','specialized']
+# print(bicycles)
+# print(bicycles[0])
+# print(bicycles[0].title())
+# m=3*2
+# print(m)
+# motorcycles=['honda','yamaha','suzuki']
+# for motorcycle in motorcycles:
+#     print(motorcycle)
+# motorcycles.append('ducati')
+# print(motorcycles)
+# motorcycles.insert(0,'ducati')
+# print(motorcycles)
+# del motorcycles[0]
+# print(motorcycles)
+# popped_motorcycle=motorcycles.pop(-2)
+# print(motorcycles)
+# print(popped_motorcycle)
+# motorcycles.remove('honda')
+# print(motorcycles)
+# motorcycles.insert(0,'honda')
+# motorcycles.insert(2,'suzuki')
+# print(motorcycles)
+# motorcycles.sort()
+# print(motorcycles)
+# motorcycles.sort(reverse=True)
+# print(motorcycles)
+# print(motorcycles.sorted())
+# print(motorcycles)
+# motorcycles.reverse()
+# print(motorcycles)
+# len(motorcycles)
+# for value in range(1,5):
+#     print(value)
+# number=list(range(1,5))
+# print(number)
+# even_number=list(range(1,11,2))
+# print(even_number)
+# squares=[]
+# for value in range(1,11):
+#     square=value**2
+#     squares.append(square)
+# print(squares)
+# squares=[value**2 for value in range(1,11)]
+# print(squares)
+# players=['charles','martina','michael','florence','eli']
+# print(players[1:3])
+# print(players[:2])
+# print(players[3:])
+# print(players[:])
+# copy_players=players[:]
+# print(copy_players)
+# dimentions=(100,200)
+# print(dimentions)
+# print(dimentions[0])
+# for dimention in dimentions:
+#     print(dimention)
+# numbers=list(range(1,11))
+# for number in numbers:
+#     if number==5:
+#         print("exist")
+#     else:
+#         print("not exist")
+# aliens_color=['green','red','yellow']
+# for alien_color in aliens_color:
+#     if alien_color=='green':
+#         print("5")
+#     elif alien_color=='red':
+#         print("10")
+#     else:
+#         print("100")
+# age=65
+# if age < 2:
+#     print("1")
+# elif age >= 2 and age < 4:
+#     print("2")
+# elif age >= 4 and age < 13:
+#     print("3")
+# elif age >= 13 and age < 20:
+#     print("4")
+# elif age >= 20 and age < 65:
+#     print("5")
+# else:
+#     print("6")
+# available_toppings=['mushrooms','olives','green peppers','pepperoni','pineapple','extra cheese']
+# requested_toppings=['mushrooms','french fries','extra cheese']
+# for requested_topping in requested_toppings:
+#     if requested_topping in available_toppings:
+#         print("adding "+requested_topping)
+#     else:
+#         print("sorry, we don't have "+requested_topping)
+# print("\nfinished making your pizza!")
